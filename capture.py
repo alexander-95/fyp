301892d64678bfda05faa3e8eae63654c82a1e06
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2015-10-13 15:33:28 +0100

    script for capturing a greyscale image

---
+++ capture.py
@@ -0,0 +1,30 @@
+import cv2
+
+cap = cv2.VideoCapture(0)
+
+#set a new resolution
+cap.set(3,640)
+cap.set(4,480)
+
+#reduce the color saturation
+cap.set(12,0.5)
+
+#change the brightness
+cap.set(10,0.5)
+
+#bump up the contrast
+cap.set(11,0.5)
+
+while(True):
+    ret, frame = cap.read()
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    #cv2.threshold(frame, 127, 255, cv2.THRESH_BINARY, frame)
+    cv2.imshow('frame', frame)
+    
+    #if the 'c' key is pressed
+    if cv2.waitKey(1) & 0xFF == ord('c'):
+        cv2.imwrite('image.png', frame)
+        break
+
+cap.release()
+cv2.destroyAllWindows()

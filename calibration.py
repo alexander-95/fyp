eea95348ceebffa67483e536b1fb9e055417317f
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2015-11-30 19:18:14 +0000

    added code for undistorting image

--- calibration.py
+++ calibration.py
@@ -11,8 +11,13 @@ HEIGHT = 9
 objp = np.zeros((WIDTH*HEIGHT,3), np.float32)
 objp[:,:2] = np.mgrid[0:HEIGHT,0:WIDTH].T.reshape(-1,2)
 
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture(1)
 
+axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
+
+x,y,w,h = [0,0,0,0]
+
+distorted = True
 while (True):
         objpoints = [] # 3d point in real world space
         imgpoints = [] # 2d points in image plane.
@@ -25,25 +30,33 @@ while (True):
 
         # Find the chess board corners
         ret, corners = cv2.findChessboardCorners(gray, (HEIGHT,WIDTH),None)
-        imgpoints.append(corners)
-        objpoints.append(objp)
         # If found, add object points, image points (after refining them)
+
         if ret == True:
-            cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
-            
+            #corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+
+            objpoints.append(objp)
+            imgpoints.append(corners)
             # Draw and display the corners
             cv2.drawChessboardCorners(img, (HEIGHT,WIDTH), corners,ret)
             #intrinsic matrix, distortion coefficients, rodrigues rotation vectors, translation vectors
             ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-            #print 'matrix =', mtx
-            print mtx.item(4)
-            #h,  w = img.shape[:2]
-            #newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-            #img = cv2.undistort(img, mtx, dist, None, newcameramtx)
+            if distorted:
+                h,  w = img.shape[:2]
+                newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
 
-        cv2.imshow('img',img)
+                # undistort
+                img = cv2.undistort(img, mtx, dist, None, newcameramtx)
 
+                # crop the image
+                x,y,w,h = roi
+                print 'a',x,y,w,h
+                img = img[y:y+h, x:x+w]
+                if w!=0:
+                    cv2.imshow('img',img)
+        print 'b',x,y,w,h
         if cv2.waitKey(1) & 0xFF == ord('q'):
+            print mtx
             break
 
 # release everything

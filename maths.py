b8d5ee8d55e7c8da9b9919aedfc257df6d78a530
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2015-12-02 15:46:43 +0000

    created function to get location

--- maths.py
+++ maths.py
@@ -13,11 +13,11 @@ def pixel_length(x1, y1, x2, f):
 
 #convert mm to px
 def px(x):
-    return x*192
+    return x*222#192
 
 #convert px to mm
 def mm(x):
-    return x/192
+    return x/222
 
 ##############################################
 # Mathematical equations for entity tracking #
@@ -27,76 +27,95 @@ def mm(x):
 def delta(x, y):
     return float(abs(x-y))
 
-#relative time between each photo
-t1 = 1.0
-t2 = 2.0
-t3 = 3.0
-t4 = 4.0
+def getLocation(l, r, f, b):
+    x1 = l[0] #object as seen from the left camera
+    y1 = l[1]
+    x2 = r[0] #object as seen from the right camera
+    y2 = r[1]
+    d1 = x1 - 320
+    dz = y1 - 240
+    d2 = x2 - 320
+    f = float(px(f))
+    b = px(b)
+    x = (f*b)/(d1-d2)
+    y = (b*d1)/(d1-d2)
+    z = x*(dz/f)
 
-#x positions of entity
-x1 = 323.0
-x2 = 184.0
-x3 = 442.0
-x4 = 314.0
+    return (mm(x),mm(y),mm(z))
 
-#y positions of entity
-y1 = 75.0
-y2 = 68.0
-y3 = 53.0
-y4 = 48.0
+def mathExample():
+    #relative time between each photo
+    t1 = 1.0
+    t2 = 2.0
+    t3 = 3.0
+    t4 = 4.0
 
-#disparities in x axis
-d1 = delta(x1, 320)
-d2 = delta(x2, 320)
-d3 = delta(x3, 320)
-d4 = delta(x4, 320)
+    #x positions of entity
+    x1 = 323.0
+    x2 = 184.0
+    x3 = 442.0
+    x4 = 314.0
 
-#disparities in y axis
-dz1 = delta(y1, 240)
-dz2 = delta(y2, 240)
-dz3 = delta(y3, 240)
-dz4 = delta(y4, 240)
+    #y positions of entity
+    y1 = 75.0
+    y2 = 68.0
+    y3 = 53.0
+    y4 = 48.0
 
-print dz1, dz2,dz3,dz4
+    #disparities in x axis
+    d1 = delta(x1, 320)
+    d2 = delta(x2, 320)
+    d3 = delta(x3, 320)
+    d4 = delta(x4, 320)
 
-b = px(80.0) #baseline
-f = px(4.0)  #focal length
+    #disparities in y axis
+    dz1 = delta(y1, 240)
+    dz2 = delta(y2, 240)
+    dz3 = delta(y3, 240)
+    dz4 = delta(y4, 240)
 
-#some extra variables
-z1 = x1*(dz1/f)
-z2 = x2*(dz2/f)
+    print dz1, dz2,dz3,dz4
 
-Vz = (z2 - z1)/delta(t2, t1)
+    b = px(80.0) #baseline
+    f = px(4.0)  #focal length
 
-A = (delta(t2, t1)*d2*(d3-d1)) - (delta(t3, t1)*d3*(d2-d1))
-C = (delta(t2, t1)*d2*(d3-d1)) - (delta(t4, t3)*d3*(d2-d4))
+    #some extra variables
+    z1 = x1*(dz1/f)
+    z2 = x2*(dz2/f)
 
-num = (A*b*(d2-d4))+(C*b*(d3-d1))
-denom = (C*(delta(t2, t1)*(d3-d1)-delta(t3, t1)*(d2-d1))) + (A*(delta(t4, t3)*(d2-d4)-delta(t4, t2)*(d3-d4)))
-Vy = float(num/denom)
+    Vz = (z2 - z1)/delta(t2, t1)
 
-num = (f*b*(d2-d4)) - (delta(t4, t3)*f*Vy*(d2-d4)) + (delta(t4, t2)*f*Vy*(d3-d4))
-denom = delta(t4, t2)*d2*(d3-d4) - delta(t4, t3)*d3*(d2-d4)
-Vx = float(num/denom)
+    A = (delta(t2, t1)*d2*(d3-d1)) - (delta(t3, t1)*d3*(d2-d1))
+    C = (delta(t2, t1)*d2*(d3-d1)) - (delta(t4, t3)*d3*(d2-d4))
 
-#position at time 1
-X1 = (((-1)*b*f)/(d2-d1)) + (delta(t2, t1)*(f*Vy - Vx*d2))/(d2-d1)
-Y1 = ((d1*delta(t2, t1)*Vy - d1*b)/(d2-d1)) - ((d1*d2*Vx*delta(t2, t1))/(f*(d2-d1)))
-Z1 = X1-(dz1/f)
+    num = (A*b*(d2-d4))+(C*b*(d3-d1))
+    denom = (C*(delta(t2, t1)*(d3-d1)-delta(t3, t1)*(d2-d1))) + (A*(delta(t4, t3)*(d2-d4)-delta(t4, t2)*(d3-d4)))
+    Vy = float(num/denom)
 
-#position at time 4
-X4 = (f*b/(d3-d4)) + ((delta(t4, t3)*(Vx*d3 - Vy*f))/(d3-d4))
-Y4 = (((b*d3)-(d4*delta(t4,t3)*Vy))/(d3-d4)) + ((delta(t4,t3)*Vx*d4*d3)/(f*(d3-d4)))
-Z4 = X4*(dz4/f)
+    num = (f*b*(d2-d4)) - (delta(t4, t3)*f*Vy*(d2-d4)) + (delta(t4, t2)*f*Vy*(d3-d4))
+    denom = delta(t4, t2)*d2*(d3-d4) - delta(t4, t3)*d3*(d2-d4)
+    Vx = float(num/denom)
 
-xdiff = delta(X1, X4)
-ydiff = delta(Y1, Y4)
-zdiff = delta(Z1, Z4)
+    #position at time 1
+    X1 = (((-1)*b*f)/(d2-d1)) + (delta(t2, t1)*(f*Vy - Vx*d2))/(d2-d1)
+    Y1 = ((d1*delta(t2, t1)*Vy - d1*b)/(d2-d1)) - ((d1*d2*Vx*delta(t2, t1))/(f*(d2-d1)))
+    Z1 = X1-(dz1/f)
 
-print mm(X1), mm(Y1), mm(Z1)
-print mm(X4), mm(Y4), mm(Z4)
-print mm(xdiff), mm(ydiff), mm(zdiff)
-#print 'height',length((460.0, 59.0), (484.0, 444.0))
-#print 'px per mm', pixel_length(100.0, 200.0, 385.0, 4.0)
-#print 'width',length((117.0, 299.0),(629.0, 291.0))
-#print 'px per mm', pixel_length(100.0, 150.0, 512.0, 4.0)
+    #position at time 4
+    X4 = (f*b/(d3-d4)) + ((delta(t4, t3)*(Vx*d3 - Vy*f))/(d3-d4))
+    Y4 = (((b*d3)-(d4*delta(t4,t3)*Vy))/(d3-d4)) + ((delta(t4,t3)*Vx*d4*d3)/(f*(d3-d4)))
+    Z4 = X4*(dz4/f)
+
+    xdiff = delta(X1, X4)
+    ydiff = delta(Y1, Y4)
+    zdiff = delta(Z1, Z4)
+
+    print mm(X1), mm(Y1), mm(Z1)
+    print mm(X4), mm(Y4), mm(Z4)
+    print mm(xdiff), mm(ydiff), mm(zdiff)
+    #print 'height',length((460.0, 59.0), (484.0, 444.0))
+    #print 'px per mm', pixel_length(100.0, 200.0, 385.0, 4.0)
+    #print 'width',length((117.0, 299.0),(629.0, 291.0))
+    #print 'px per mm', pixel_length(100.0, 150.0, 512.0, 4.0)
+
+mathExample()

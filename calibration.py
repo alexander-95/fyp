a8b19199c8963277eb245fa0c91de39f11e95b78
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2015-12-05 23:44:46 +0000

    added code for estimating camera matrix

--- calibration.py
+++ calibration.py
@@ -11,53 +11,49 @@ HEIGHT = 9
 objp = np.zeros((WIDTH*HEIGHT,3), np.float32)
 objp[:,:2] = np.mgrid[0:HEIGHT,0:WIDTH].T.reshape(-1,2)
 
-cap = cv2.VideoCapture(1)
+cap = cv2.VideoCapture()
+cap.open('http://192.168.0.5:8080/video?.mjpeg')
 
 axis = np.float32([[3,0,0], [0,3,0], [0,0,-3]]).reshape(-1,3)
 
-x,y,w,h = [0,0,0,0]
+camera = []
 
-distorted = True
 while (True):
-        objpoints = [] # 3d point in real world space
-        imgpoints = [] # 2d points in image plane.
+    objpoints = [] # 3d point in real world space
+    imgpoints = [] # 2d points in image plane.
 
-        #capture a frame
-        ret, img = cap.read()
+    #capture a frame
+    ret, img = cap.read()
 
-        # OUr operations on the frame come here
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Find the chess board corners
-        ret, corners = cv2.findChessboardCorners(gray, (HEIGHT,WIDTH),None)
-        # If found, add object points, image points (after refining them)
+    # Find the chess board corners
+    ret, corners = cv2.findChessboardCorners(gray, (HEIGHT,WIDTH),None)
+    # If found, add object points, image points (after refining them)
 
-        if ret == True:
-            #corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
+    if ret == True:
+        #corners2 = cv2.cornerSubPix(gray,corners,(11,11),(-1,-1),criteria)
 
-            objpoints.append(objp)
-            imgpoints.append(corners)
-            # Draw and display the corners
-            cv2.drawChessboardCorners(img, (HEIGHT,WIDTH), corners,ret)
-            #intrinsic matrix, distortion coefficients, rodrigues rotation vectors, translation vectors
-            ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
-            if distorted:
-                h,  w = img.shape[:2]
-                newcameramtx, roi=cv2.getOptimalNewCameraMatrix(mtx,dist,(w,h),1,(w,h))
-
-                # undistort
-                img = cv2.undistort(img, mtx, dist, None, newcameramtx)
-
-                # crop the image
-                x,y,w,h = roi
-                print 'a',x,y,w,h
-                img = img[y:y+h, x:x+w]
-                if w!=0:
-                    cv2.imshow('img',img)
-        print 'b',x,y,w,h
-        if cv2.waitKey(1) & 0xFF == ord('q'):
+        objpoints.append(objp)
+        imgpoints.append(corners)
+        # Draw and display the corners
+        cv2.drawChessboardCorners(img, (HEIGHT,WIDTH), corners,ret)
+        #intrinsic matrix, distortion coefficients,
+        #rodrigues rotation vectors, translation vectors
+        ret, mtx, dist, rvecs, tvecs = cv2.calibrateCamera(objpoints, imgpoints, gray.shape[::-1],None,None)
+        if cv2.waitKey(1) & 0xFF == ord(' '):
             print mtx
-            break
+            camera.append(mtx)
+    cv2.imshow('img',img)
+    #print 'b',x,y,w,h
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        num = float(len(camera))
+        camera = sum(camera)
+        camera = float(camera/num)
+        print "matrix estimation:",camera
+        print mtx
+        break
 
 # release everything
 cap.release()

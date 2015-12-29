3d8f899e94cdcc3afc774a6b002b170b5933407d
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2015-12-29 00:03:01 +0000

    added function for finding objects

--- live_tracker.py
+++ live_tracker.py
@@ -20,6 +20,17 @@ frame2 = cv2.GaussianBlur(frame2,(5,5),0)
 cv2.threshold(frame1, 40, 255, cv2.THRESH_BINARY, frame1)
 cv2.threshold(frame2, 40, 255, cv2.THRESH_BINARY, frame2)
 
+def findObjects(frame):
+    contours, hierarchy = cv2.findContours(frame.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
+    moments = [cv2.moments(cnt) for cnt in contours]
+    centroids = [(int (round(m['m10']/m['m00'])), int(round(m['m01']/m['m00']))) for m in moments if m['m00']!=0]
+    centroids.pop()
+    for c in centroids:
+        cv2.circle(frame,c,5,(255,0,0))
+
+findObjects(frame1)
+findObjects(frame2)
+
 while True:
     frame3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2GRAY)
     frame4 = cv2.cvtColor(frame4, cv2.COLOR_BGR2GRAY)
@@ -29,11 +40,16 @@ while True:
     
     cv2.threshold(frame3, 40, 255, cv2.THRESH_BINARY, frame3)
     cv2.threshold(frame4, 40, 255, cv2.THRESH_BINARY, frame4)
+    
+    findObjects(frame3)
+    findObjects(frame4)
 
     cv2.imshow('frame1',frame1)
     cv2.imshow('frame2',frame2)
     cv2.imshow('frame3',frame3)
     cv2.imshow('frame4',frame4)
+    
+
     if cv2.waitKey(1) & 0xFF == ord('c'):
         break
     frame1 = frame3

305f3f78dd0e89f1d065219221c2c4ef4adbb12d
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2016-03-10 13:49:21 +0000

    added color to centroid circles

--- live_tracker.py
+++ live_tracker.py
@@ -7,13 +7,13 @@ from maths import getMetrics, getLocation, getPosition
 res = [640, 480]
 kernel = np.ones((5,5),np.uint8)
 
-capL = cv2.VideoCapture(0)
+capL = cv2.VideoCapture(2)
 #capL.open('http://192.168.43.180:8081/video.mjpeg')
 #capL.open('http://192.168.1.117:8081/video.mjpeg')
 capL.set(3,res[0])
 capL.set(4,res[1])
 
-capR = cv2.VideoCapture(2)
+capR = cv2.VideoCapture(0)
 #capR.open('http://192.168.43.231:8081/video.mjpeg')
 #capR.open('http://192.168.1.116:8081/videp.mjpeg')
 capR.set(3,res[0])
@@ -52,10 +52,6 @@ def findObjects(frame):
     centroids = [(int (round(m['m10']/m['m00'])), int(round(m['m01']/m['m00']))) for m in moments if m['m00']!=0]
     if len(centroids)>0:
         centroids.pop()
-    #cv2.line(frame, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
-    #cv2.line(frame, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
-    for c in centroids:
-        cv2.circle(frame,c,5,(255,0,0))
     return centroids
 
 l1 = findObjects(frame1)
@@ -79,6 +75,10 @@ while True:
     cv2.line(frame3, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
     cv2.line(frame4, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
     cv2.line(frame4, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
+    for centroid in l2:
+        cv2.circle(frame3,centroid,5,(255,0,0))
+    for centroid in r2:
+        cv2.circle(frame4,centroid,5,(255,0,0))
 
     cv2.imshow('frame1',frame1)
     cv2.imshow('frame2',frame2)

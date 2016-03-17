6277d700736270eee413e4dd5ca69754aafab60b
Alexander Mitchell <alex.mitchell.1995@gmail.com>
2016-03-17 16:44:47 +0000

    code cleanup

--- live_tracker.py
+++ live_tracker.py
@@ -4,10 +4,12 @@ import numpy as np
 from maths import getMetrics, getLocation, getPosition
 
 #res = [1280, 720]
-delay = 0.0
+delay = 0.1
 blurVal = 13
 counter = 0
 res = [640, 480]
+crosshairs_enabled = True
+centroids_enabled = True
 
 capL = cv2.VideoCapture(1)
 #capL.open('http://192.168.43.180:8081/video.mjpeg')
@@ -15,7 +17,7 @@ capL = cv2.VideoCapture(1)
 capL.set(3,res[0])
 capL.set(4,res[1])
 
-capR = cv2.VideoCapture(2)
+capR = cv2.VideoCapture(0)
 #capR.open('http://192.168.43.231:8081/video.mjpeg')
 #capR.open('http://192.168.1.116:8081/videp.mjpeg')
 capR.set(3,res[0])
@@ -39,50 +41,36 @@ ret, frame4 = capR.read()
 t[3] = time()
 sleep(delay)
 
-frame1 = cv2.cvtColor(frame1, cv2.COLOR_BGR2GRAY)
-frame2 = cv2.cvtColor(frame2, cv2.COLOR_BGR2GRAY)
-frame1 = cv2.GaussianBlur(frame1,(blurVal,blurVal),0)
-frame2 = cv2.GaussianBlur(frame2,(blurVal,blurVal),0)
-cv2.threshold(frame1, 30, 255, cv2.THRESH_BINARY, frame1)
-cv2.threshold(frame2, 30, 255, cv2.THRESH_BINARY, frame2)
-frame1 = cv2.morphologyEx(frame1, cv2.MORPH_CLOSE, kernel)
-frame2 = cv2.morphologyEx(frame2, cv2.MORPH_CLOSE, kernel)
-
 def findObjects(frame):
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    frame = cv2.GaussianBlur(frame,(blurVal,blurVal),0)
+    cv2.threshold(frame, 50, 255, cv2.THRESH_BINARY, frame)
+    frame = cv2.morphologyEx(frame, cv2.MORPH_CLOSE, kernel)
     contours, hierarchy = cv2.findContours(frame.copy(), cv2.RETR_LIST,cv2.CHAIN_APPROX_NONE)
     moments = [cv2.moments(cnt) for cnt in contours]
     centroids = [(int (round(m['m10']/m['m00'])), int(round(m['m01']/m['m00']))) for m in moments if m['m00']!=0]
     if len(centroids)>0:
         centroids.pop()
     return centroids
+
 l1 = findObjects(frame1)
 r1 = findObjects(frame2)
 
 while True:
-    frame3 = cv2.cvtColor(frame3, cv2.COLOR_BGR2GRAY)
-    frame4 = cv2.cvtColor(frame4, cv2.COLOR_BGR2GRAY)
-    
-    frame3 = cv2.GaussianBlur(frame3,(blurVal,blurVal),0)
-    frame4 = cv2.GaussianBlur(frame4,(blurVal,blurVal),0)
-    
-    cv2.threshold(frame3, 50, 255, cv2.THRESH_BINARY, frame3)
-    cv2.threshold(frame4, 50, 255, cv2.THRESH_BINARY, frame4)
-    
-    frame3 = cv2.morphologyEx(frame3, cv2.MORPH_CLOSE, kernel)
-    frame4 = cv2.morphologyEx(frame4, cv2.MORPH_CLOSE, kernel)
-
     l2 = findObjects(frame3)
     r2 = findObjects(frame4)
-    frame3 = cv2.cvtColor(frame3, cv2.COLOR_GRAY2BGR)
-    frame4 = cv2.cvtColor(frame4, cv2.COLOR_GRAY2BGR)
-    cv2.line(frame3, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
-    cv2.line(frame3, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
-    cv2.line(frame4, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
-    cv2.line(frame4, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
-    for centroid in l2:
-        cv2.circle(frame3,centroid,5,(255,255,0),thickness=2)
-    for centroid in r2:
-        cv2.circle(frame4,centroid,5,(255,255,0),thickness=2)
+    
+    if crosshairs_enabled:
+        cv2.line(frame3, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
+        cv2.line(frame3, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
+        cv2.line(frame4, (0, res[1]/2), (res[0], res[1]/2), (0, 0, 255))
+        cv2.line(frame4, (res[0]/2, 0), (res[0]/2, res[1]), (0, 0, 255))
+    
+    if centroids_enabled:    
+        for centroid in l2:
+            cv2.circle(frame3,centroid,5,(255,255,0),thickness=2)
+        for centroid in r2:
+            cv2.circle(frame4,centroid,5,(255,255,0),thickness=2)
     
     f1 = cv2.resize(frame1, (0,0), fx=0.5, fy=0.5) 
     f2 = cv2.resize(frame2, (0,0), fx=0.5, fy=0.5)
@@ -95,7 +83,12 @@ while True:
     cv2.imshow('frame4',f4)
     
     if cv2.waitKey(1) & 0xFF == ord(' '):
-        cv2.imwrite('frame1.png',frame1)
+        cv2.imwrite('frame1.png', frame1)
+        cv2.imwrite('frame2.png', frame2)
+        cv2.imwrite('frame3.png', frame3)
+        cv2.imwrite('frame4.png', frame4)
+        print l1, l2, r1, r2, t
+    
     if len(l1) == 1 and len(r1) == 1 and len(l2) == 1 and len(r2) == 1:
         print
         counter+=1
